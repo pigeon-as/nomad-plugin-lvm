fd85f3524aeb0864826be46ffcc75f906bab9f3a
Joakim Bakke Hellum <joakim.hellum@protonmail.ch>
2026-03-04 11:56:19 +0100

    fix bin lookup

--- cmd/nomad-plugin-lvm/main.go
+++ cmd/nomad-plugin-lvm/main.go
@@ -8,18 +8,25 @@ import (
 )
 
 func main() {
+	// Nomad DHV plugins are launched with a stripped environment (only DHV_*
+	// variables). Ensure PATH is set so exec.LookPath can find system binaries.
+	if os.Getenv("PATH") == "" {
+		os.Setenv("PATH", "/usr/sbin:/usr/bin:/sbin:/bin")
+	}
+
 	req, err := lvm.ParseRequest()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
 	}
 
-	binPath := req.Params.BinPath
-	if binPath == "" {
-		binPath = lvm.DefaultBinPath
+	client, err := lvm.NewClient(lvm.ExecCommand{})
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		os.Exit(1)
 	}
 
-	p := lvm.NewPlugin(lvm.NewClient(lvm.ExecCommand{}, binPath))
+	p := lvm.NewPlugin(client)
 	if err := p.Run(req); err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n", err)
 		os.Exit(1)
